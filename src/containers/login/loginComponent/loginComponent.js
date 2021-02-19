adca22e1da707ee1142495fe677a661c341b23b2
Oksana Smolynets <oksana.mavdryk25@gmail.com>
2021-02-19 16:46:40 +0200

    login form validation

--- src/containers/login/loginComponent/loginComponent.js
+++ src/containers/login/loginComponent/loginComponent.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { TextField, Typography, Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Typography } from '@material-ui/core';
 
 import './loginComponent.scss';
 
 export const LoginComponent = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data);
+    formClear();
+    alert(`Email:${data.Email}, Password: ${data.Password}`);
+  };
+
+  console.log(errors);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function formClear() {
+    setEmail('');
+    setPassword('');
+  }
+
   return (
     <div className="login">
-      <form noValidate autoComplete="off">
-        <Typography variant="h6">LOGIN</Typography>
-        <TextField id="email" label="Email adress" variant="outlined" />
-        <TextField id="password" label="Password" variant="outlined" />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => {
-            alert('clicked');
-          }}>
-          LOG IN
-        </Button>
-        <p>Forgot your password?</p>
-        <p>Don’t have an account yet? </p>
-        <Button href="#text-buttons" color="primary">
-          Register
-        </Button>
-      </form>{' '}
+      <Typography variant="h6">Login</Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          placeholder="Email"
+          name="Email"
+          ref={register({ required: true, minLength: 1, pattern: /^\S+@\S+$/i })}
+        />
+
+        <input
+          type="text"
+          placeholder="Password"
+          name="Password"
+          ref={register({
+            required: true,
+            minLength: 1,
+            maxLength: 30,
+            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/i,
+          })}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
